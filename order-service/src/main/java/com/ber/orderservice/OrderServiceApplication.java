8a932fca0c8ba4c48f4e554463ebefa33e695e0b
ayoubberachad <ayoub.berrachad@gmail.com>
2023-02-21 01:05:10 +0100

    order-service

---
+++ order-service/src/main/java/com/ber/orderservice/OrderServiceApplication.java
@@ -0,0 +1,69 @@
+package com.ber.orderservice;
+
+import com.ber.orderservice.entities.Order;
+import com.ber.orderservice.entities.ProductItem;
+import com.ber.orderservice.enums.OrderStatus;
+import com.ber.orderservice.model.Customer;
+import com.ber.orderservice.model.Product;
+import com.ber.orderservice.repository.OrderRepository;
+import com.ber.orderservice.repository.ProductItemRepository;
+import com.ber.orderservice.services.CustomerRestClientService;
+import com.ber.orderservice.services.InventoryRestClientService;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Random;
+
+
+@SpringBootApplication
+@EnableFeignClients
+public class OrderServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderServiceApplication.class, args);
+    }
+    @Bean
+    CommandLineRunner start(
+            OrderRepository orderRepository,
+            ProductItemRepository productItemRepository,
+            CustomerRestClientService customerRestClientService,
+            InventoryRestClientService inventoryRestClientService){
+        return args -> {
+            List<Customer> customers = new ArrayList<>();
+            List<Product> products= new ArrayList<>();
+            customerRestClientService.allCustomers().forEach(customers::add);
+            inventoryRestClientService.allProducts().forEach(products::add);
+            Long customerId=1L;
+            Random random=new Random();
+            Customer customer=customerRestClientService.customerById(customerId);
+            for (int i = 0; i <20 ; i++) {
+                Order order=Order.builder()
+                        .customerId(customers.get(random.nextInt(customers.size())).getId())
+                        .status(Math.random()>0.5? OrderStatus.PENDING:OrderStatus.CREATED)
+                        .createdAt(new Date())
+                        .build();
+                Order savedOrder = orderRepository.save(order);
+                for (int j = 0; j < products.size() ; j++) {
+                    if(Math.random()>0.70){
+                        ProductItem productItem=ProductItem.builder()
+                                .order(savedOrder)
+                                .productId(products.get(j).getId())
+                                .price(products.get(j).getPrice())
+                                .quantity(1+random.nextInt(10))
+                                .discount(Math.random())
+                                .build();
+                        productItemRepository.save(productItem);
+                    }
+
+                }
+            }
+        };
+    }
+
+}
